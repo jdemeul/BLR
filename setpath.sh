0334d8a15350b1698ae22cb124cf89add1557002
Tobias Frick <hans.tobias.frick@gmail.com>
2018-11-30 13:33:58 +0100

    sbatch compability
    Adding BLR folder to paths.txt to be fetched by BLR_automation.sh since sbatch is not compatible with $(dirname $0), (otherwise used).

--- setpath.sh
+++ setpath.sh
@@ -17,11 +17,12 @@ while getopts "he" OPTION; do
 		    echo ''
 			echo 'This script creates paths.txt containing the paths to external software used by the WGH pipeline'
 			echo ""
-			echo 'Useage: bash set_path.sh <picard_path> <Bowtie2_ref> <fragScaff_path>'
+			echo 'Useage: bash set_path.sh <picard_path> <Bowtie2_ref> <BLR_folder>'
 			echo ''
 			echo "Positional arguments (required)"
-			echo "  <picard_path>       Path to picard tools jar file"
+			echo "  <picard_path>       Path to picard tools jar file (e.g. /picard_2.5.0/picard.jar)"
 			echo "  <Bowtie2_ref>       Path to bowtie2 reference (e.g. /Bowtie2Index/genome)"
+			echo "  <BLR_path           Path to BLR folder (e.g. /GitHub_repos/BLR)"
 			echo ""
 			echo "Optional arguments"
 			echo "  -h  help (this output)"
@@ -38,13 +39,14 @@ done
 
 ARG1=${@:$OPTIND:1}
 ARG2=${@:$OPTIND+1:1}
+ARG2=${@:$OPTIND+2:1}
 
-if [ -z "$ARG1" ] || [ -z "$ARG2" ]
+if [ -z "$ARG1" ] || [ -z "$ARG2" ] || [ -z "$ARG3" ]
 then
     echo ""
     echo "ARGUMENT ERROR"
     echo "Did not find all three positional arguments, see -h for more information."
-    echo "(got picard_path:"$ARG1" and Bowtie2_ref:"$ARG2" instead)"
+    echo "(got picard_path:"$ARG1", Bowtie2_ref:"$ARG2" and BLR_folder:"$ARG3" instead)"
     echo ""
     exit 0
 fi
@@ -66,12 +68,20 @@ then
     ARG2=$work_dir'/'$ARG2
 fi
 
+elif [[ "$ARG3" != /* ]]
+then
+    echo 'set_paths: BLR_folder supplied as relative, writing as absolute'
+    work_dir=$(pwd)
+    ARG3=$work_dir'/'$ARG3
+fi
+
 #
 # Writing files
 #
 
-wgh_path=$(dirname "$0")
+wgh_path=$(ARG3)
 echo 'set_paths: Creating paths.txt in your WGH_Analysis folder'
 
 printf 'picard_path='$ARG1 > $wgh_path/paths.txt
 printf '\nbowtie2_reference='$ARG2 >> $wgh_path/paths.txt
+printf '\nwgh_path='$ARG3 >> $wgh_path/paths.txt
